ae10abe163db554d907b896e5473d1fb539701ea
AnetM <mudruk.anya@icloud.com>
2022-09-07 12:34:30 +0300

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,22 +1,38 @@
-// const enterNumber = prompt("Введите число!");
-let input = prompt("Введите число!");
-let total = 0;
-input = Number(input);
-// total += input;
-// console.log(input);
-// console.log(total);
+// // const enterNumber = prompt("Введите число!");
+// let input = prompt("Введите число!");
+// let total = 0;
+// input = Number(input);
+// // total += input;
+// // console.log(input);
+// // console.log(total);
 
 
-if (input !== NaN) {
-    total += input;
-    console.log(total);
-    input = prompt("Введите число!");
-} else if (Number(input) === NaN) {
-    alert(`Было введено не число, попробуйте еще раз`);
+// if (input !== NaN) {
+//     total += input;
+//     console.log(total);
+//     input = prompt("Введите число!");
+// } else if (Number(input) === NaN) {
+//     alert(`Было введено не число, попробуйте еще раз`);
+// }
+// else {
+//     alert(`Общая сумма чисел равна ${total}`);
+// }
+
+
+function formatMessage(message, maxLength) {
+  let result;
+  // Change code below this line
+  if (message.length < maxLength) {
+    result = message;
+  } else {
+    result = message.slice(0, maxLength) + '...';
+  }
+
+  /// Change code above this line
+
+  return result;
 }
-else {
-    alert(`Общая сумма чисел равна ${total}`);
-}
 
+console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41))
 
 
